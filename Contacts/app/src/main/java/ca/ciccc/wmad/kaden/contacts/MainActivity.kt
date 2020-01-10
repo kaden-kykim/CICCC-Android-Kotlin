172ded78bd83175cf619658a3a5be98d27a13e9e
kaden-kykim <kaden.kykim@gmail.com>
2020-01-10 15:07:56 -0800

    Add Contact API Service with Retrofit/Gson through directly Co-routine   - Need to implement Job in ViewModel

--- Contacts/app/src/main/java/ca/ciccc/wmad/kaden/contacts/MainActivity.kt
+++ Contacts/app/src/main/java/ca/ciccc/wmad/kaden/contacts/MainActivity.kt
@@ -1,7 +1,7 @@
 package ca.ciccc.wmad.kaden.contacts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
---
+++ Contacts/app/src/main/java/ca/ciccc/wmad/kaden/contacts/network/ContactApiService.kt
@@ -0,0 +1,26 @@
+package ca.ciccc.wmad.kaden.contacts.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://randomuser.me/api/"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface ContactApiService {
+    @GET("?nat=ca")
+    fun getContacts(@Query("results") num: Int): Deferred<String>
+}
+
+object ContactApi {
+    val retrofitService :
+            ContactApiService by lazy { retrofit.create(ContactApiService::class.java) }
+}
