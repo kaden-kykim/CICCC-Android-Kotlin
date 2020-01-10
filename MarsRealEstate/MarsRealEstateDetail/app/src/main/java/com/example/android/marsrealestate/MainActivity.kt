5e97c72c4d9faeaa2215e69330ddb4a8c0e1721b
kaden-kykim <kaden.kykim@gmail.com>
2020-01-10 11:53:14 -0800

    Finish Tutorial [08.3 Filtering and detail views with internet data]

--- MarsRealEstate/MarsRealEstateDetail/app/src/main/java/com/example/android/marsrealestate/MainActivity.kt
+++ MarsRealEstate/MarsRealEstateDetail/app/src/main/java/com/example/android/marsrealestate/MainActivity.kt
@@ -19,6 +19,8 @@ package com.example.android.marsrealestate
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,5 +31,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationUI.setupActionBarWithNavController(this,
+                findNavController(R.id.nav_host_fragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
